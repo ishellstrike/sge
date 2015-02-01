2ca90e46e54aa0a7a148df478e7f2898976f2c87
ishellstrike <ishellstrike@gmail.com>
2015-02-01 20:51:37 +0300

    aftermerge

--- shader.h
+++ shader.h
@@ -12,13 +12,8 @@ public:
     std::string name;
     std::vector<int> vars;
     void Use() const;
-<<<<<<< HEAD
     GLint locateVars(const std::string &s);
     void PushGlobalHeader(const std::string &s, const char *newParameter = "#version 120");
-=======
-    GLint locateVar(const std::string &s);
-    void PushGlobalHeader(const std::string &s, const char *newParameter = "#version 210");
->>>>>>> 352992101835dc6a114c459c82272cd3ebc100b6
     void loadShaderFromSource(GLenum type, const std::string &source);
     bool Link();
     GLint program;
