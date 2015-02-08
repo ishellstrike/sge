c52b49d9300309c07e08c97cef3ea563db77a236
ishellstrike <ishellstrike@gmail.com>
2015-02-09 02:09:08 +0300

    noise

---
+++ ClassicNoise.h
@@ -0,0 +1,8 @@
+#pragma once
+#ifndef CLASSNOISE_H
+#define CLASSNOISE_H
+float noise( float x, float y, float z );
+float simplexnoise( float x, float y, float z, float w );
+float simplexnoise( float xin, float yin, float zin );
+float simplexnoise( float xin, float yin );
+#endif
