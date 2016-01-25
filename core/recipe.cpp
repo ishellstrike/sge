31dec5a63f1399c5c6d18989e9a588b586ecbbf7
Andrey Samsonov <ishellstrike@gmail.com>
2016-01-25 19:07:37 +0300

    Create recipe.cpp

---
+++ core/recipe.cpp
@@ -0,0 +1,17 @@
+#include "recipe.h"
+#include "core/agents/chest.h"
+
+void Deserialize(const rapidjson::Value &val)
+{
+    
+}
+
+void IsReady(const Chest &chest) const
+{
+
+}
+
+void Craft(const Chest &chest) const
+{
+
+}
